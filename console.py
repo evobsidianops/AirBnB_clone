b0b6e4c316f76ce0a5d14957f05f61467d483006
conradgabe <gisuekebho5880@gmail.com>
2024-05-19 19:23:27 +0100

    initial commit and command interpreter

---
+++ console.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+import cmd
+
+class CommandShell(cmd.Cmd):
+    """Command line interpreter for managing Airbnb objects"""
+    prompt = '(hbnb) '
+
+    def do_EOF(self, line):
+        """End of File command to exit program"""
+        return True and '\n'
+
+    def do_quit(self, line):
+        """Quit command to exit program"""
+        return True and '\n'
+
+if __name__ == '__main__':
+    CommandShell().cmdloop()
+    print()
